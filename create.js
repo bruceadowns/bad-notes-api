07b631558f68ac26c2df8bbae0e9bce5f82c5bb3
Bruce Downs <bruce.downs@autodesk.com>
2018-12-28 10:03:28 -0800

    Making a mistake

--- create.js
+++ create.js
@@ -1,8 +1,7 @@
+gibberish.what;
 import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
-
-gibberish.what;
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
